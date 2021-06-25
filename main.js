eb5e4a60cef2c032104be315007f0b77b6e5c4af
annie020483 <86432124+annie020483@users.noreply.github.com>
2021-06-25 11:29:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function flipimage() {
+    document.getElementById("i1").innerHTML.src="close-up-of-tulips-blooming-in-field-royalty-free-image-1584131603.jpg";
+}
+function changecolor(){
+    document.getElementById("h1").style.color="green";
+}
